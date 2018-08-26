4395ef5c4736fe991f05c089e3725b82a4ed77e2
Sam <30577766+Samasaur1@users.noreply.github.com>
2018-08-25 20:49:39 -0400

    Fix install script
    Use mv instead of cp. For some reason, cp wouldn't make a new file in /usr/local/bin

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 curl -LO https://github.com/Samasaur1/TyperTool/releases/download/v1.0.0/typer-1.0.0.zip
 unzip typer-1.0.0.zip
-cp typer /usr/local/bin/typer
+mv typer /usr/local/bin/typer
 rm typer typer-1.0.0.zip
